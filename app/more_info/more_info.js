ee77f563f959b9ba409908fd39fd5316b08a5c31
Raashul <rashul1996@gmail.com>
2017-03-21 01:55:19 -0400

    Changed brand text

--- app/more_info/more_info.js
+++ app/more_info/more_info.js
@@ -2,58 +2,58 @@
 	angular.module('Rashul')
 		.controller('MoreInfoController',[ '$scope', '$http', '$state', '$interval', "$location" , function($scope, $http, $state, $interval, $location){
 
-				if(localStorage.getItem('user_name')== null || localStorage.getItem('user_name')== undefined){
+			if(localStorage.getItem('user_name')== null || localStorage.getItem('user_name')== undefined){
 				$scope.no_user_logged_in = true;
 			}
 
 
 
-			 var request = {
-				id : $location.search().id,
-				send_to: localStorage.getItem('user_email')
-			  }
+		 var request = {
+			id : $location.search().id,
+			send_to: localStorage.getItem('user_email')
+		  }
 
-			$http.post('api/info/get', request).success(function(response){
+		$http.post('api/info/get', request).success(function(response){
+
+			//response is the Post Information we received
+			$scope.post = response;
+
+		}).error(function(err){
+			console.log(err);
+		})
+
+
+		// $scope.contactSeller = function(req,res){
+
+		//     $http.get('api/test').success(function(response){
+		//         console.log('hello');
+		//         res.send('hi');
+		//     });
+
+		// }
+
+		$scope.sendEmailToSeller = function(){
+
+			$http.post('api/sendEmail', request).success(function(response){
+
+				console.log(response);
 
 				//response is the Post Information we received
-				$scope.post = response;
+				var data = response;
+				var email = response.email;
+
+				$scope.email_sent_alert = true;
+
 
 			}).error(function(err){
+
 				console.log(err);
+				$location.path('/');
 			})
+		}
 
 
-			// $scope.contactSeller = function(req,res){
-
-			//     $http.get('api/test').success(function(response){
-			//         console.log('hello');
-			//         res.send('hi');
-			//     });
-
-			// }
-
-			$scope.sendEmailToSeller = function(){
-
-				$http.post('api/sendEmail', request).success(function(response){
-
-					console.log(response);
-
-					//response is the Post Information we received
-					var data = response;
-					var email = response.email;
-
-					$scope.email_sent_alert = true;
-
-
-				}).error(function(err){
-
-					console.log(err);
-					$location.path('/');
-				})
-			}
-
-
-	}])
+}])
 
 
 
